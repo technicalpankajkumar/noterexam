8ad2bb7848ee899902b47b2e75b5ebbcace230fa
pankajkumar <pankajkumar@unicodesystems.info>
2025-07-17 13:01:05 +0530

    added flash screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,9 @@
 import { useAuth } from '@contexts/AuthContext';
 import { LinearGradient } from 'expo-linear-gradient';
 import { useRouter } from 'expo-router';
+import { NotebookPen } from 'lucide-react-native';
 import React, { useEffect } from 'react';
-import { ActivityIndicator, Text, View } from 'react-native';
+import { View } from 'react-native';
 
 export default function SplashScreen() {
   const { user, loading } = useAuth();
@@ -25,15 +26,19 @@ export default function SplashScreen() {
       colors={['#4A90E2', '#357ABD']}
       className={'flex-1'}
     >
-      <View className={'flex-1 justify-center items-center px-5'}>
-        <Text className={'text-white text-3xl font-bold mb-2'}>
-          Welcome
-        </Text>
-        <Text className={'text-white text-opacity-80 text-base mb-10'}>
-          Noter-Exam
-        </Text>
-        <ActivityIndicator size="large" color="white" className={'mt-5'} />
+     <View className="flex-1 bg-blue-600 items-center justify-center">
+      {/* Concentric Circles */}
+      <View className="absolute w-[500px] h-[500px] rounded-full bg-blue-500 border border-dotted border-white animate-spin" />
+      <View className="absolute w-96 h-96 rounded-full bg-blue-400 opacity-45 border border-dotted border-blue-800 animate-spin" />
+      <View className="absolute w-64 h-64 rounded-full bg-blue-200 opacity-35" />
+      <View className="absolute w-28 h-w-28 rounded-full bg-blue-100 opacity-25" />
+
+      {/* Center Icon + Text */}
+      <View className="w-24 h-24 rounded-full border-4 border-white items-center justify-center bg-blue-500">
+        <NotebookPen size={32} color="white" />
+        
       </View>
+    </View>
     </LinearGradient>
   );
 }
